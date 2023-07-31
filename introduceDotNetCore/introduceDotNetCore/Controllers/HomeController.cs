011849a598115a6a5d57515fbcf847a5e917e0ef
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-07-31 12:05:51 +0300

    basic intro about .net core

---
+++ introduceDotNetCore/introduceDotNetCore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using introduceDotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace introduceDotNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AllProducts()
+        {
+            var products = new List<Product>
+            {
+                new(){ Id =1, Name="X", Description="Desc X", Price= 10 },
+                new(){ Id =1, Name="Y", Description="Desc Y", Price= 10 },
+                new(){ Id =1, Name="Z", Description="Desc Z", Price= 10 },
+
+            };
+            return View(products);
+        }
+    }
+}
--- introduceDotNetCore/introduceDotNetCore/Program.cs
+++ introduceDotNetCore/introduceDotNetCore/Program.cs
@@ -1,6 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession(option => option.IdleTimeout = TimeSpan.FromMinutes(20));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+/*
+ * Middleware - pipeline
+ *   1. Şişeyi soda ile doldur
+ *   2. Şişeye etiket bas -> Middleware *  
+ *   3. Şişeyi kapat
+ *   4. Şişeyi koliye yerleştir.
+ *   
+ *   Middleware: ne yapacağım?
+ *   Pipe-line: hangi sırayla yapacağım?
+ */
+
+app.UseStaticFiles();
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();

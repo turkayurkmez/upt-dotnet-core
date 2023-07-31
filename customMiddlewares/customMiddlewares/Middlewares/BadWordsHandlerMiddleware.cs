1d5261c67e83a49fc4d88466ad8aa5b8690f0241
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-07-31 16:34:49 +0300

    custom middleware with exception

---
+++ customMiddlewares/customMiddlewares/Middlewares/BadWordsHandlerMiddleware.cs
@@ -0,0 +1,30 @@
+﻿namespace customMiddlewares.Middlewares
+{
+    public class BadWordsHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BadWordsHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var jsonBody = (string?)context.Items["jsonBody"];
+            var badwords = new List<string> { "pis", "kaka", "kötü", "deli" };
+            if (jsonBody != null && badwords.Any(word => jsonBody.Contains(word)))
+            {
+                context.Response.StatusCode = 400;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(new { message = "Bu gönderide hoş olmayan kelimeler var!" });
+
+                return;
+
+            }
+
+            await _next(context);
+
+        }
+    }
+}
---
+++ customMiddlewares/customMiddlewares/Middlewares/JsonBodyMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace customMiddlewares.Middlewares
+{
+    public class JsonBodyMiddleware
+    {
+        /*
+         * Eğer bir http request nesnesi POST metodu ile çalışmış ve bu request'de JSON bir veri varsa daha sonra işlemek üzere bu veriyi ayır!
+         */
+        private readonly RequestDelegate _next;
+
+        public JsonBodyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+            if (context.Request.Method == "POST" && context.Request.ContentType.StartsWith("application/json"))
+            {
+                using var streamReader = new StreamReader(context.Request.Body);
+                var jsonBody = await streamReader.ReadToEndAsync();
+                context.Items["jsonBody"] = jsonBody;
+            }
+            context.Request.Body.Seek(0, SeekOrigin.Begin);
+            await _next(context);
+        }
+    }
+}
